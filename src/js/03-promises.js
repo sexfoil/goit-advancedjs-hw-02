11bde3fcc554e6134c8aabc2cc5b303db29a518c
Slava Poliakov <78slavapolyakov@gmail.com>
2023-11-12 17:58:07 +0200

    Finished 3rd task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,97 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});        
+      } else {
+        reject({position, delay});        
+      }
+    }, delay);
+  });
+
+  return promise;
+}
+
+function getValidationMessage(name, value) {
+  return `'${name}' must be not empty and more than ${value}!`;
+} 
+
+const elements = {
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  button: document.querySelector('button')
+}
+
+function showValidationAlert(message) {
+    iziToast.show({
+        title: '⛔',
+        message: message,
+        messageColor: 'white',
+        backgroundColor: 'red',
+        timeout: 5000,
+        position: 'center'
+    });
+}
+
+function showPromiseAlert(message, backgroundColor) {
+  iziToast.show({
+      message: message,
+      backgroundColor: backgroundColor,
+      position: 'topRight'
+  });
+}
+
+function isInputValid({ delay, step, amount }) {
+  let errorMessage = "";
+
+  if (!delay || delay < 0) {
+    errorMessage += getValidationMessage("delay", -1) + "<br>"
+  }
+  
+  if (!step || step < 0) {
+    errorMessage += getValidationMessage("step", -1) + "<br>"
+  }
+  
+  if (!amount || amount <= 0) {
+    errorMessage += getValidationMessage("amount", 0) + "<br>"
+  }
+  
+  if (errorMessage.length === 0) {
+    return true;
   } else {
-    // Reject
+    showValidationAlert(errorMessage);
+    return false;
   }
 }
+
+const onButtonClick = (evt) => {
+  evt.preventDefault();
+  
+  const userInput = {
+    delay: elements.delay.value,
+    step: elements.step.value,
+    amount: elements.amount.value
+  }
+
+  if (isInputValid(userInput)) {
+    for (let position = 1; position <= Number(userInput.amount); position += 1) {
+      const delay = Number(userInput.delay) + (position - 1) * Number(userInput.step);
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          showPromiseAlert(`✅ Fulfilled promise ${position} in ${delay}ms`, 'lime');
+        })
+        .catch(({ position, delay }) => {
+          showPromiseAlert(`❌ Rejected promise ${position} in ${delay}ms`, 'tomato');
+        });
+    }
+  }
+}
+
+elements.button.addEventListener('click', onButtonClick);
